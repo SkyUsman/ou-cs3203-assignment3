76bfcdf511aac5c5e9180487017ee1d7201d1585
Usman Syed <83891637+SkyUsman@users.noreply.github.com>
2023-09-13 17:11:46 -0500

    new function --> "multiply_list"

--- main.py
+++ main.py
@@ -9,5 +9,19 @@ def sum_list(numbers):
 
     return sum 
 
+# Computes the product of list elements
+def multiply_list(numbers):
+
+    if len(numbers) > 0:
+        product = 1
+    else:
+        product = 0
+
+    for value in numbers:
+        product *= value
+    
+    return product
+
 numbers = [1, 5, 10, 4, 5]
 print("Sum of list ---> ", sum_list(numbers))
+print("Product of list ---> ", multiply_list(numbers))
